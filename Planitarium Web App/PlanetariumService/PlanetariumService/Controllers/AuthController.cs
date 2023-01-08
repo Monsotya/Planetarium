ad0d7a900f53bd282af5c7bd4ac7c45af2ac82d7
Денис Б <denisbereznuuk@gmail.com>
2023-01-08 21:50:13 +0200

    "Sign Up" & "Sign In" pages fix. AuthController refactor

--- Planitarium Web App/PlanetariumService/PlanetariumService/Controllers/AuthController.cs
+++ Planitarium Web App/PlanetariumService/PlanetariumService/Controllers/AuthController.cs
@@ -38,23 +38,25 @@ namespace PlanetariumService.Controllers
             {
                 // var userdetails = await _context.Userdetails
                 // .SingleOrDefaultAsync(m => m.Email == model.Email && m.Password == model.Password);
-                Users user = null; 
+                /*Users user = null; 
                 try {
                     user = userService.GetByUsername(model.Login);
                 } catch (AggregateException e) {
                     //ModelState.AddModelError("Login", "Invalid Username.");
                     ViewBag.LoginError = "Invalid Username.";
                     return RedirectToAction(nameof(SignIn));
-                }
-                /*if (user == null)
-                {
-                    ModelState.AddModelError("Login", "Invalid Username.");
-                    return RedirectToAction(nameof(SignIn));
                 }*/
+                var user = userService.GetByUsername(model.Login);
+                if (user == null)
+                {
+                    // ModelState.AddModelError("Login", "Invalid Username.");
+                    TempData["LoginError"] = "Invalid Username.";
+                    return RedirectToAction(nameof(SignIn));
+                }
                 if (user.UserPassword != model.Password)
                 {
                     //ModelState.AddModelError("Password", "Invalid Sign in attempt.");
-                    ViewBag.PasswordError = "Invalid Sign in attempt. (Wrong Password)";
+                    TempData["PasswordError"] = "Invalid Sign in attempt. (Wrong Password)";
                     return RedirectToAction(nameof(SignIn));
                 }
                 HttpContext.Session.SetString("userId", user.Username);
@@ -79,16 +81,21 @@ namespace PlanetariumService.Controllers
         [Route("Signup")]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register() {//(RegistrationViewModel model)
+        public async Task<ActionResult> Register(RegisterUI model) {//(RegistrationViewModel model)
             if (ModelState.IsValid)
             {
+                if (userService.GetByUsername(model.Username) != null)
+                {
+                    TempData["UsernameError"] = "Invalid Username.";
+                    return RedirectToAction(nameof(SignUp));
+                }
                 Users user = new Users
                 {
-                    // Username = model.Username,
+                    Username = model.Username,
                     /*Name = model.Name,
                     Lastame = model.Lastname,*/
-                    // Email = model.Email,
-                    // UserPassword = model.Password
+                    Email = model.Email,
+                    UserPassword = model.UserPassword
 
                 };
                 var res = userService.Add(user);
